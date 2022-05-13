29c2e2250e7ae766784cb2b92644d3b149033cce
Ihor110986 <ihor110986@gmail.com>
2022-05-14 00:23:01 +0300

    Add public function 'latest_news' and update my_module.routing.yml

--- web/modules/custom/my_module/src/Controller/MyController.php
+++ web/modules/custom/my_module/src/Controller/MyController.php
@@ -6,4 +6,21 @@ class MyController {
             '#markup' => 'Welcome to our Website.'
         );
     }
+
+    public function latest_news() {
+        $myEntytyStorage = \Drupal::entityTypeManager()->getStorage('News');
+        $data = $myEntytyStorage->getQuery()
+            ->condition('status', 1)
+            ->condition('type', 'News')
+            ->sort('created', 'DESC')
+            ->range(0,1)
+            ->execute();
+        $entity_type = 'News';
+        $view_mode = 'Full content';
+        $creator = \Drupal::entityTypeManager()->getViewBuilder($entity_type);
+        $storage = \Drupal::entityTypeManager()->getStorage($entity_type);
+        $news = $storage->load(reset($data));
+        $create = $creator->view($news, $view_mode);
+        return $create;
+    }
 }
