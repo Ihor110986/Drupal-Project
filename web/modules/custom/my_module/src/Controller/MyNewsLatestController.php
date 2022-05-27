e02f6fca66651eedb8a5fd501fc200492967fde5
Ihor110986 <ihor110986@gmail.com>
2022-05-28 00:53:23 +0300

    Add the latest news output

--- web/modules/custom/my_module/src/Controller/MyNewsLatestController.php
+++ web/modules/custom/my_module/src/Controller/MyNewsLatestController.php
@@ -1,22 +1,22 @@
 <?php
-//namespace Drupal\my_module\Controller;
-//
-//class MyNewsLatestController {
-//    public function latest_news() {
-//        $myEntytyStorage = \Drupal::entityTypeManager()->getStorage('node');
-//        $data = $myEntytyStorage->getQuery()
-//            ->condition('status', 1)
-//            ->condition('type', 'news')
-//            ->sort('created', 'DESC')
-//            ->range(0,1)
-//            ->execute();
-//        $entity_type = 'node';
-//        $view_mode = 'teaser';
-//        $builder = \Drupal::entityTypeManager()->getViewBuilder($entity_type);
-//        $storage = \Drupal::entityTypeManager()->getStorage($entity_type);
-//        $news = $storage->load(reset($data));
-//        $create = $builder->view($news, $view_mode);
-//        return $create;
-//    }
-//}
-//?>
+namespace Drupal\my_module\Controller;
+
+class MyNewsLatestController {
+    public function latest_news() {
+        $myEntytyStorage = \Drupal::entityTypeManager()->getStorage('node');
+        $data = $myEntytyStorage->getQuery()
+            ->condition('status', 1)
+            ->condition('type', 'news')
+            ->sort('created', 'DESC')
+            ->range(0,1)
+            ->execute();
+        $entity_type = 'node';
+        $view_mode = 'teaser';
+        $builder = \Drupal::entityTypeManager()->getViewBuilder($entity_type);
+        $storage = \Drupal::entityTypeManager()->getStorage($entity_type);
+        $news = $storage->load(reset($data));
+        $create = $builder->view($news, $view_mode);
+        return $create;
+    }
+}
+?>
