b091363da92dc305df0a9a902fff06dedfd0b015
Ihor110986 <ihor110986@gmail.com>
2022-05-30 15:25:35 +0300

    Add My first template

--- web/modules/custom/my_module/src/Controller/MyModuleTemplateController.php
+++ web/modules/custom/my_module/src/Controller/MyModuleTemplateController.php
@@ -1,14 +1,14 @@
 <?php
-//namespace Drupal\my_module\Controller;
-//
-//use Drupal\Core\Controller\ControllerBase;
-//
-//class MyModuleTemplateController extends ControllerBase {
-//    public function content() {
-//
-//        return array(
-//            '#theme' => 'my-module-template',
-//            '#test_var' => $this->t('Test Value'),
-//        );
-//    }
-//}
+namespace Drupal\my_module\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+class MyModuleTemplateController extends ControllerBase {
+    public function content() {
+
+        return array(
+            '#theme' => 'my-module-template',
+            '#test_var' => $this->t('Test Value'),
+        );
+    }
+}
