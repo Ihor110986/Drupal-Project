94f0be93437d971708e0e51aed8c3bb916eda710
Ihor110986 <ihor110986@gmail.com>
2022-05-30 11:16:49 +0300

    Add filter news and output news

--- web/modules/custom/my_module/src/Form/MyModuleConfigNewsOrderForm.php
+++ web/modules/custom/my_module/src/Form/MyModuleConfigNewsOrderForm.php
@@ -13,31 +13,32 @@ class MyModuleConfigNewsOrderForm extends ConfigFormBase {
   /**
    * {@inheritdoc}
    */
-  public function getFormId() {
-    return 'order_news_form';
+  protected function getEditableConfigNames(): array {
+    return [
+      'my_module_news_filter.settings',
+    ];
   }
 
   /**
    * {@inheritdoc}
    */
-  protected function getEditableConfigNames() {
-    return [
-      'my_module.settings',
-    ];
+  public function getFormId() {
+    return 'filter_news_form';
   }
 
   /**
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
-    $config = $this->config('my_module.settings');
-    $form['filter_field'] = [
-      '#type' => 'checkboxes',
-      '#title' => $this->t('News order:'),
+    $config = $this->config('my_module_news_filter.settings');
+
+    $form['filter_field'] = array(
+      '#type' => 'radios',
+      '#title' => $this->t('News filter:'),
       '#options' => array('created' => 'By create date', 'changed' => 'By update date'),
-      '#default_value' => $config->get('news_order'),
+      '#default_value' => $config->get('news_filter'),
       '#required' => TRUE,
-    ];
+    );
     return parent::buildForm($form, $form_state);
   }
 
@@ -45,8 +46,8 @@ class MyModuleConfigNewsOrderForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-    $this->config('my_module.settings')
-      ->set('news_order', $form_state->getValue('filter_field'))
+    $this->config('my_module_news_filter.settings')
+      ->set('news_filter', $form_state->getValue('filter_field'))
       ->save();
     parent::submitForm($form, $form_state);
   }
