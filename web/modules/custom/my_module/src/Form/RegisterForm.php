df71f47576a481d2b797778f5a53ee08d7129096
Ihor110986 <ihor110986@gmail.com>
2022-05-31 16:19:36 +0300

    Add register form to event and add output this form in block

--- web/modules/custom/my_module/src/Form/RegisterForm.php
+++ web/modules/custom/my_module/src/Form/RegisterForm.php
@@ -1,55 +1,55 @@
 <?php
-//
-///**
-// * @file
-// * Contains \Drupal\my_module\Form\RegisterForm.
-// */
-//
-//namespace Drupal\my_module\Form;
-//
-//use Drupal\Core\Form\FormBase;
-//use Drupal\Core\Form\FormStateInterface;
-//
-//class RegisterForm extends FormBase {
-//  /**
-//   * {@inheritdoc}
-//   */
-//  public function getFormId() {
-//    return 'register_form';
-//  }
-//
-//  /**
-//   * {@inheritdoc}
-//   */
-//  public function buildForm(array $form, FormStateInterface $form_state) {
-//
-//    $form['your_name'] = array(
-//      '#type' => 'textfield',
-//      '#title' => t('Your name:'),
-//      '#required' => TRUE,
-//    );
-//
-//    $form['your_email'] = array(
-//      '#type' => 'email',
-//      '#title' => t('Your email:'),
-//      '#required' => TRUE,
-//    );
-//
-//    $form['actions']['#type'] = 'actions';
-//    $form['actions']['submit'] = array(
-//      '#type' => 'submit',
-//      '#value' => $this->t('Register'),
-//      '#button_type' => 'primary',
-//    );
-//    return $form;
-//  }
-//
-//  /**
-//   * {@inheritdoc}
-//   */
-//  public function submitForm(array &$form, FormStateInterface $form_state) {
-//
-//    \Drupal::messenger()->addMessage($this->t('Thank you @you_name, you are registered!', array('@you_name' => $form_state->getValue('your_name'))));
-//
-//  }
-//}
+
+/**
+ * @file
+ * Contains \Drupal\my_module\Form\RegisterForm.
+ */
+
+namespace Drupal\my_module\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+class RegisterForm extends FormBase {
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'register_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+
+    $form['your_name'] = array(
+      '#type' => 'textfield',
+      '#title' => t('Your name:'),
+      '#required' => TRUE,
+    );
+
+    $form['your_email'] = array(
+      '#type' => 'email',
+      '#title' => t('Your email:'),
+      '#required' => TRUE,
+    );
+
+    $form['actions']['#type'] = 'actions';
+    $form['actions']['submit'] = array(
+      '#type' => 'submit',
+      '#value' => $this->t('Register'),
+      '#button_type' => 'primary',
+    );
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+
+    \Drupal::messenger()->addMessage($this->t('Thank you @you_name, you are registered!', array('@you_name' => $form_state->getValue('your_name'))));
+
+  }
+}
