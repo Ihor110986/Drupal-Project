5e3d7cc4d12aac044607718882c17fe5e20b4450
Ihor110986 <ihor110986@gmail.com>
2022-06-03 22:57:12 +0300

    Change value 'False' by hook_post_update

---
+++ web/modules/custom/my_product/my_product.post_update.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Add new custom page
+ */
+function my_product_post_update_create_page(&$sandbox) {
+  \Drupal\node\Entity\Node::create([
+    'type' => 'page',
+    'title' => 'Some page',
+    'status' => 1,
+  ])->save();
+  \Drupal::messenger()->addMessage('Add page');
+}
+
+/**
+ * Set FALSE value from flag field
+ */
+function my_product_post_update_set_value_flag(&$sandbox) {
+  $storage = \Drupal::entityTypeManager()->getStorage('node');
+  $ids = $storage->getQuery()
+    ->condition('type', 'product')
+    ->execute();
+
+  $nodes = \Drupal\node\Entity\Node::loadMultiple($ids);
+  foreach ($nodes as $node) {
+    $node->set('flag', FALSE);
+    $node->save();
+  }
+}
