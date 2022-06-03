b2a79cb6a6f38cc4bcccf7ce63f63fe83fd1fb61
Ihor110986 <ihor110986@gmail.com>
2022-06-03 18:37:34 +0300

    Create new_module_batch

---
+++ web/modules/custom/new_module_batch/src/Form/MyBatchForm.php
@@ -0,0 +1,92 @@
+<?php
+
+
+namespace Drupal\new_module_batch\Form;
+
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+class MyBatchForm extends FormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+
+  public function getFormId(): string {
+    return 'new_module_batch_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $storage = \Drupal::entityTypeManager()->getStorage('node');
+    $nodeIds = $storage->getQuery()
+      ->condition('type', 'news')
+      ->condition('field_archive', 0)
+      ->execute();
+    $nodes = $storage->loadMultiple($nodeIds);
+    $output = [];
+    foreach ($nodes as $node) {
+      $output[] = [
+        'id' => $node->id(),
+        'title' => $node->label(),
+      ];
+    }
+    $header = [
+      'title' => $this->t('Node`s title'),
+    ];
+    $form['table'] = [
+      '#type' => 'tableselect',
+      '#header' => $header,
+      '#options' => $output,
+      '#empty' => t('No node`s found'),
+    ];
+    $form['actions'] = [
+      '#type' => 'actions',
+    ];
+    $form['actions']['submit'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Submit'),
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $nodes = $form['table']['#options'];
+    $selected = $form_state->getValue('table');
+    $i = 0;
+    $operations = [];
+    foreach ($nodes as $node) {
+      if (is_string($selected[$i])) {
+        $operations[] = [
+          '\Drupal\new_module_batch\Form\MyBatchForm::toArchive',
+          [$node['id']],
+        ];
+      }
+      $i++;
+    }
+    batch_set([
+      'title' => $this->t('Archive selected News'),
+      'operations' => $operations,
+    ]);
+  }
+
+  /**
+   * Node to archive checked
+   */
+  public static function toArchive($params) {
+    $nodeStorage = \Drupal::entityTypeManager()->getStorage('node');
+    $node = $nodeStorage->load($params);
+    $node->set('field_archive', 1);
+    $node->save();
+  }
+
+}
