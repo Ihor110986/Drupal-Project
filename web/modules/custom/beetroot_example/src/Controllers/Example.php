abb0ec4277ae2d3f435ab6987e430f391a2d6d57
Ihor110986 <ihor110986@gmail.com>
2022-06-07 16:15:59 +0300

    Add entityTypeManager to class Example

--- web/modules/custom/beetroot_example/src/Controllers/Example.php
+++ web/modules/custom/beetroot_example/src/Controllers/Example.php
@@ -8,12 +8,24 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 class Example extends ControllerBase {
 
   public function view() {
-    $users = $this->entityTypeManager()->getStorage('user')->loadMultiple();
-    $names = [];
-    foreach ($users as $user) {
-      $names[] = $user->label();
+    $en = $this->entityTypeManager();
+    $storage = $en->getStorage('node');
+    $query = $storage->getQuery()
+      ->condition('status', 1)
+      ->condition('type', 'article')
+      ->condition('field_tags.entity.field_show_on_home', True)
+      ->range(0,1)
+      ->sort('created', 'DESC');
+    $ids = $query->execute();
+    /**@var \Drupal\node\NodeInterface $node */
+    $node = $storage->load(reset($ids));
+
+    if ($node->hasField('field_flag')) {
+      $body = $node->get('body')->value;
+
     }
-    return new JsonResponse(['hello' => 'world', 'users' => $names]);
+    return new JsonResponse(['hello' => 'world']);
   }
 
 }
+
