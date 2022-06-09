546567ba1c14bdf48796e1ea81e17b504978987a
Ihor110986 <ihor110986@gmail.com>
2022-06-09 12:58:43 +0300

    Update admin side to site

--- web/modules/custom/beetroot_example/src/Controllers/Example.php
+++ web/modules/custom/beetroot_example/src/Controllers/Example.php
@@ -4,26 +4,14 @@ namespace Drupal\beetroot_example\Controllers;
 
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\node\NodeInterface;
-use Symfony\Component\HttpFoundation\JsonResponse;
+
 
 class Example extends ControllerBase {
 
   public function view(NodeInterface $node) {
-    $em = $this->entityTypeManager();
-
-    $storage = $em->getStorage( 'node');
-
-    $query = $storage->getQuery()
-      ->condition('status', 1)
-      ->condition('type', 'article')
-//      ->condition('field_tags.entity.field_show_on_home', TRUE)
-      ->range(0, 1)
-      ->sort('changed', 'DESC');
-    $ids = $query->execute();
-    /** @var \Drupal\node\NodeInterface $node */
-    $node = $storage->load(reset($ids));
-
-    return new JsonResponse($node->toArray());
+    return $this->entityTypeManager()
+      ->getViewBuilder($node->getEntityTypeId())
+      ->viewField($node->get('body'),'teaser');
   }
 }
 
