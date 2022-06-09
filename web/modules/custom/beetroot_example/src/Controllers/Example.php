3db6e5f7984cc5e867c4251b6a8d299e65a712d0
Ihor110986 <ihor110986@gmail.com>
2022-06-10 00:15:03 +0300

    Add ExampleForm

--- web/modules/custom/beetroot_example/src/Controllers/Example.php
+++ web/modules/custom/beetroot_example/src/Controllers/Example.php
@@ -1,17 +1,30 @@
 <?php
 
+
 namespace Drupal\beetroot_example\Controllers;
 
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\node\NodeInterface;
 
-
+/**
+ * Shows example of controller for Academy.
+ */
 class Example extends ControllerBase {
 
+  /**
+   * Shows Node's body field.
+   *
+   * @param \Drupal\node\NodeInterface $node
+   *   The node to show it's body field.
+   *
+   * @return array
+   *   Node view renderable array.
+   */
   public function view(NodeInterface $node) {
     return $this->entityTypeManager()
       ->getViewBuilder($node->getEntityTypeId())
-      ->viewField($node->get('body'),'teaser');
+      ->viewField($node->get('body'), 'teaser');
   }
+
 }
 
